1a9e60d79de6721aa236609b36d85652ca6f9615
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-04-08 23:15:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,10 @@ def reformat_languages(languages)
 reformatted = {}
   languages.each do |category, language|
     language.each do |name, attributes|
-     reformatted[name] ||= attributes
-     reformatted[name][:style] = []
-     reformatted[name][:style] << category
+      reformatted[name] ||= attributes
+      reformatted[name][:style] = []
+      reformatted[name][:style] << category
+      reformatted[java][:style] << category
     end
   end
   reformatted
