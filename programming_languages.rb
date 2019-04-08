289e4a66d53dae2c4ec06fe5e2d90c271d019356
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-04-08 22:56:19 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,8 @@ new_languages = {}
   languages.each do |style, language|
     language.each do |name, attributes|
     new_languages[name] ||= attributes
-    new_languages[name][:style]= []
-    new_languages[name][:style]<< style
+    new_languages[name][:style] = []
+    new_languages[name][:style] << style
     end
   end
   new_languages
